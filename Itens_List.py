5855d26c7152950d4af3f3a503cd7e282a30edcf
siriusthewolf <rvsantos97@gmail.com>
2020-01-03 01:46:29 -0300

    Add Items List Script

---
+++ Itens_List.py
@@ -0,0 +1,10 @@
+# Listing of items armors and weapons
+# Each item is a dictionary with the characteristics given in the DnD 3.5 books:
+# Weapon Name - Price (in gold) - Damage (Small) - Damage (Medium) - Critical - Range (in meters) - Weight (in Kilos)
+
+# WEAPONS
+
+Club = {"Price": 0, "Damage S": "d4", "Damage M ": "d6", "Critical": 2, "Range": 1.5, "Weight": 1.5}
+Dagger = {"Price": 2, "Damage S": "d3", "Damage M": "d4", "Critical": 2, "Range": 1.5, "Weight": 0.5}
+Heavy_Mace = {"Price": 12, "Damage S": "d6", "Damage M": "d8", "Critical": 2, "Range": 1.5, "Weight": 5}
+Morningstar = {"Price": 8, "Damage S": "d6", "Damage M": "d8", "Critical": 2, "Range": 1.5, "Weight": 3.5}
