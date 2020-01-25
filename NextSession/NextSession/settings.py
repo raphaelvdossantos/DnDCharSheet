dd0a6c97e7fa435e3df5b21bd7b9174cebee8a29
siriusthewolf <rvsantos97@gmail.com>
2020-01-24 22:22:20 -0300

    Configured Djongo for MongoDB usage

--- NextSession/NextSession/settings.py
+++ NextSession/NextSession/settings.py
@@ -78,8 +78,9 @@ WSGI_APPLICATION = 'NextSession.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'RollingDices',
+
     }
 }
 
--- NextSession/main/models.py
+++ NextSession/main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from datetime import datetime
+import django.utils
 
 # Create your models here.
 class CharSheet(models.Model):
     char_name = models.CharField(max_length=100)
     char_shortbio = models.TextField()
-    char_date = models.DateTimeField("Data de Criação do Personagem", default=datetime.now())
+    char_date = models.DateTimeField("Data de Criação do Personagem", default=django.utils.timezone.now)
 
     def __str__(self):
         return self.char_name
