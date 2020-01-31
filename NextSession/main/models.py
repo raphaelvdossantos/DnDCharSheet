9f56962d98a141167a92e59d992e2423b2f002c8
siriusthewolf <rvsantos97@gmail.com>
2020-01-30 22:45:00 -0300

    Added columns and More tables

--- NextSession/main/models.py
+++ NextSession/main/models.py
@@ -15,3 +15,33 @@ class CharDescription(models.Model):
 
     def __str__(self):
         return self.char_name
+
+
+class BaseStatus(models.Model):
+    c_str = models.IntegerField()
+    c_dex = models.IntegerField()
+    c_con = models.IntegerField()
+    c_int = models.IntegerField()
+    c_wis = models.IntegerField()
+    c_cha = models.IntegerField()
+
+
+class CombatStatus(models.Model):
+    Life_Points = models.IntegerField()
+    weapon_one = models.CharField(max_length= 15, blank=True, null=True)
+    weapon_two = models.CharField(max_length= 15, blank=True, null=True)
+    weapon_three = models.CharField(max_length= 15, blank=True, null=True)
+    weapon_four = models.CharField(max_length= 15, blank=True, null=True)
+
+    shield = models.CharField(max_length= 15, blank=True, null=True)
+    armor = models.CharField(max_length= 15, blank=True, null=True)
+    protection_one = models.CharField(max_length= 15, blank=True, null=True)
+    protection_two = models.CharField(max_length= 15, blank=True, null=True)
+
+
+
+
+
+
+
+
